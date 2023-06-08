f6da6e47569431b7226b13aef7610f08329e3eb2
limjh-morai <jhlim@morai.ai>
2023-06-08 16:10:34 +0900

    fix_name

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# erp42_blanks
-erp42 예제에 대한 공백 코드
+# erp42_answer
+erp42 예제에 대한 정답 코드
+
+버전 내역은 https://github.com/MORAI-EDU/erp42_answer/tags 참조
--- scripts/velodyne_parser.py
+++ scripts/velodyne_parser.py
@@ -42,7 +42,7 @@ class SCANParser:
 
             angle = np.arctan2(point[1], point[0])
 
-            if point[0] > 0 and point[2] > -1.3 and dist < 50:
+            if point[0] > 0 and point[2] > -0.65 and dist < 50:
                 point_list.append((point[0], point[1], point[2], point[3], dist, angle))
 
         point_np = np.array(point_list, np.float32)
